5feaae7fdc353762e94e9aec9f08b492d7be41e9
Emilie Pessoa Rodrigues <emiliepessoarodrigues@gmail.com>
2026-03-10 21:29:43 -0300

    Aula 10.03

---
+++ Desenvolvimento de sistemas - Luciano/multi-pages/src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+// import App from './App.jsx'
+
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import { Home } from './pages/Home';
+import { Sobre } from "./pages/Sobre";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/sobre",
+    element: <Sobre />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+    {/* <App /> */}
+  </StrictMode>,
+);
