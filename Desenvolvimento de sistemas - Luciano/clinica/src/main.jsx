f60b0e1ed9809a299fc0338d100193d2a435a3d5
LwBane <emiliepessoa8@gmail.com>
2026-04-07 21:51:27 -0300

    feat: estrutura inicial do projeto clinica com rpages e começo do LoginForm

---
+++ Desenvolvimento de sistemas - Luciano/clinica/src/main.jsx
@@ -0,0 +1,32 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+// ==> React Router 
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+// ==> Toastify
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ToastContainer />
+    <RouterProvider router={router} />
+  </StrictMode>
+)
