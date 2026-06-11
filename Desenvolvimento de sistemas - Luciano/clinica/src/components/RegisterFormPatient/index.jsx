cc2f96d041e03e247ce37f5482b845d1b039cf2b
LwBane <emiliepessoa8@gmail.com>
2026-06-10 21:40:24 -0300

    feat: complementa o formulário de cadastro do paciente

--- Desenvolvimento de sistemas - Luciano/clinica/src/components/RegisterFormPatient/index.jsx
+++ Desenvolvimento de sistemas - Luciano/clinica/src/components/RegisterFormPatient/index.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import axios from 'axios'
 import { toast } from 'react-toastify'
 
+import { IMaskInput } from 'react-imask';
+
 function RegisterFormPatient() {
     const [formData, setFormData] = useState({
         fullName: "",
@@ -66,7 +68,7 @@ function RegisterFormPatient() {
                 }
             }))
         } catch (error) {
-            console.log("Erro ao buscar endereço, error", error)
+            console.log("Erro ao buscar endereço", error)
         }
     }
 
@@ -136,8 +138,8 @@ function RegisterFormPatient() {
             className='space-y-6 text-gray-800'
             autoComplete='off'
         >
-
             <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+
                 {/* Nome completo */}
                 <fieldset>
                     <label htmlFor='fullName' className='block text-sm font-medium mb-1'>Nome Completo</label>
@@ -152,11 +154,9 @@ function RegisterFormPatient() {
                     />
                 </fieldset>
 
+                {/* Gênero */}
                 <fieldset>
-
-
                     <label htmlFor='gender' className='block text-sm font-medium mb-1'>Gênero</label>
-
                     <select
                         name='gender'
                         value={formData.gender}
@@ -168,13 +168,314 @@ function RegisterFormPatient() {
                         <option value="masculino">Masculino</option>
                         <option value="feminino">Feminino</option>
                         <option value="outro">Outro</option>
-
-
                     </select>
-
                 </fieldset>
 
+                {/* Data de nascimento */}
+                <fieldset>
+                    <label htmlFor='birthdate' className='block text-sm font-medium mb-1'>Data de nascimento</label>
+                    <input
+                        type='date'
+                        name='birthdate'
+                        id='birthdate'
+                        value={formData.birthdate}
+                        onChange={handleInputChange}
+                        required
+                        className='w-full border p-2 rounded-lg focus:ring-2 focus:ring-cyan-600 outline-none'
+                    />
+                </fieldset>
+
+                {/* CPF */}
+                <fieldset>
+                    <label htmlFor='cpf' className='block text-sm font-medium mb-1'>CPF</label>
+                    <IMaskInput
+                        mask="000.000.000-00" // Máscara de entrada: formato pré definido
+                        name="cpf"
+                        minLength={14}
+                        id="cpf"
+                        value={formData.cpf}
+                        onAccept={(value) => setFormData((prev) => ({ ...prev, cpf: value }))}
+                        className='w-full border p-2 rounded-lg focus:ring-2 focus:ring-cyan-600 outline-none'
+                    />
+                </fieldset>
+
+                {/* RG */}
+                <fieldset>
+                    <label htmlFor='rg' className='block text-sm font-medium mb-1'>RG</label>
+                    <input
+                        type='text'
+                        name='rg'
+                        id='rg'
+                        value={formData.rg}
+                        onChange={handleInputChange}
+                        required
+                        className='w-full border p-2 rounded-lg focus:ring-2 focus:ring-cyan-600 outline-none'
+                    />
+                </fieldset>
+
+                {/* Estado civil */}
+                <fieldset>
+                    <label htmlFor='maritalStatus' className='block text-sm font-medium mb-1'>Estado Civil</label>
+                    <select
+                        name='maritalStatus'
+                        value={formData.maritalStatus}
+                        onChange={handleInputChange}
+                        required
+                        className='w-full border p-2 rounded-lg focus:ring-2 focus:ring-cyan-600 outline-none'
+                    >
+                        <option value="">Selecione</option>
+                        <option value="solteiro(a)">Solteiro(a)</option>
+                        <option value="casado(a)">Casado(a)</option>
+                        <option value="divorciado(a)">Divorciado(a)</option>
+                        <option value="viuvo(a)">Viúvo(a)</option>
+                    </select>
+                </fieldset>
+
+                {/* Telefone */}
+                <fieldset>
+                    <label htmlFor='phone' className='block text-sm font-medium mb-1'>Telefone</label>
+                    <IMaskInput
+                        mask="(00) 00000-0000"
+                        name="phone"
+                        id="phone"
+                        value={formData.phone}
+                        onAccept={(value) => setFormData((prev) => ({ ...prev, phone: value }))}
+                        className='w-full border p-2 rounded-lg focus:ring-2 focus:ring-cyan-600 outline-none'
+                    />
+                </fieldset>
+
+                {/* Contato de emergêcia */}
+                <fieldset>
+                    <label htmlFor='emergencyContact' className='block text-sm font-medium mb-1'>Contato de emergêcia</label>
+                    <IMaskInput
+                        mask="(00) 00000-0000"
+                        name="emergencyContact"
+                        id="emergencyContact"
+                        value={formData.emergencyContact}
+                        onAccept={(value) => setFormData((prev) => ({ ...prev, emergencyContact: value }))}
+                        className='w-full border p-2 rounded-lg focus:ring-2 focus:ring-cyan-600 outline-none'
+                    />
+                </fieldset>
+
+                {/* E-mail */}
+                <fieldset>
+                    <label htmlFor='email' className='block text-sm font-medium mb-1'>Email</label>
+                    <input
+                        type='email'
+                        name='email'
+                        id='email'
+                        value={formData.email}
+                        onChange={handleInputChange}
+                        required
+                        className='w-full border p-2 rounded-lg focus:ring-2 focus:ring-cyan-600 outline-none'
+                    />
+                </fieldset>
+
+                {/* Naturalidade */}
+                <fieldset>
+                    <label htmlFor='birthplace' className='block text-sm font-medium mb-1'>Naturalidade:</label>
+                    <input
+                        type='text'
+                        name='birthplace'
+                        id='birthplace'
+                        value={formData.birthplace}
+                        onChange={handleInputChange}
+                        required
+                        className='w-full border p-2 rounded-lg focus:ring-2 focus:ring-cyan-600 outline-none'
+                    />
+                </fieldset>
+
+
+                {/* Alergias */}
+                <fieldset>
+                    <label htmlFor='allergies' className='block text-sm font-medium mb-1'>Alergias?</label>
+                    <input
+                        type='text'
+                        name='allergies'
+                        id='allergies'
+                        value={formData.allergies}
+                        onChange={handleInputChange}
+                        className='w-full border p-2 rounded-lg focus:ring-2 focus:ring-cyan-600 outline-none'
+                    />
+                </fieldset>
+
+
+                {/* Cuidados especiais */}
+                <fieldset>
+                    <label htmlFor='specialCare' className='block text-sm font-medium mb-1'>Cuidados Especiais?</label>
+                    <input
+                        type='text'
+                        name='specialCare'
+                        id='specialCare'
+                        value={formData.specialCare}
+                        onChange={handleInputChange}
+                        className='w-full border p-2 rounded-lg focus:ring-2 focus:ring-cyan-600 outline-none'
+                    />
+                </fieldset>
+
+
+                {/* Convênio */}
+                <fieldset>
+                    <label htmlFor='healthInsurance' className='block text-sm font-medium mb-1'>Convênio</label>
+                    <input
+                        type='text'
+                        name='healthInsurance'
+                        id='healthInsurance'
+                        value={formData.healthInsurance}
+                        onChange={handleInputChange}
+                        className='w-full border p-2 rounded-lg focus:ring-2 focus:ring-cyan-600 outline-none'
+                    />
+                </fieldset>
+
+
+                {/* Número do Convênio */}
+                <fieldset>
+                    <label htmlFor='insuranceNumber' className='block text-sm font-medium mb-1'>Número do Convênio</label>
+                    <input
+                        type='text'
+                        name='insuranceNumber'
+                        id='insuranceNumber'
+                        value={formData.insuranceNumber}
+                        onChange={handleInputChange}
+                        className='w-full border p-2 rounded-lg focus:ring-2 focus:ring-cyan-600 outline-none'
+                    />
+                </fieldset>
+
+
+                {/* Validade do Convênio */}
+                <fieldset>
+                    <label htmlFor='insuranceValidity' className='block text-sm font-medium mb-1'>Validade do Convênio</label>
+                    <input
+                        type='date'
+                        name='insuranceValidity'
+                        id='insuranceValidity'
+                        value={formData.insuranceValidity}
+                        onChange={handleInputChange}
+                        className='w-full border p-2 rounded-lg focus:ring-2 focus:ring-cyan-600 outline-none'
+                    />
+                </fieldset>
+
+
+                {/* CEP */}
+                <fieldset>
+                    <label htmlFor='cep' className='block text-sm font-medium mb-1'>CEP</label>
+                    <IMaskInput
+                        mask="00000-000"
+                        name='cep'
+                        id='cep'
+                        value={formData.address.cep}
+                        onAccept={(value) => handleAddressChange({ target: { name: "cep", value } })}
+                        onBlur={handleCepBlur}
+                        className='w-full border p-2 rounded-lg focus:ring-2 focus:ring-cyan-600 outline-none'
+                    />
+                </fieldset>
+
+                {/* Rua */}
+                <fieldset>
+                    <label htmlFor='street' className='block text-sm font-medium mb-1'>Rua</label>
+                    <input
+                        type='text'
+                        name='street'
+                        id='street'
+                        value={formData.address.street}
+                        onChange={handleAddressChange}
+                        className='w-full border p-2 rounded-lg focus:ring-2 focus:ring-cyan-600 outline-none'
+                    />
+                </fieldset>
+
+                {/* Número */}
+                <fieldset>
+                    <label htmlFor='number' className='block text-sm font-medium mb-1'>Número</label>
+                    <input
+                        type='text'
+                        name='number'
+                        id='number'
+                        value={formData.address.number}
+                        onChange={handleAddressChange}
+                        className='w-full border p-2 rounded-lg focus:ring-2 focus:ring-cyan-600 outline-none'
+                    />
+                </fieldset>
+
+                {/* Complemento */}
+                <fieldset>
+                    <label htmlFor='complement' className='block text-sm font-medium mb-1'>Complemento</label>
+                    <input
+                        type='text'
+                        name='complement'
+                        id='complement'
+                        value={formData.address.complement}
+                        onChange={handleAddressChange}
+                        className='w-full border p-2 rounded-lg focus:ring-2 focus:ring-cyan-600 outline-none'
+                    />
+                </fieldset>
+
+                {/* Referência */}
+                <fieldset>
+                    <label htmlFor='reference' className='block text-sm font-medium mb-1'>Referência</label>
+                    <input
+                        type='text'
+                        name='reference'
+                        id='reference'
+                        value={formData.address.reference}
+                        onChange={handleAddressChange}
+                        className='w-full border p-2 rounded-lg focus:ring-2 focus:ring-cyan-600 outline-none'
+                    />
+                </fieldset>
+
+                {/* Bairro */}
+                <fieldset>
+                    <label htmlFor='neighborhood' className='block text-sm font-medium mb-1'>Bairro</label>
+                    <input
+                        type='text'
+                        name='neighborhood'
+                        id='neighborhood'
+                        value={formData.address.neighborhood}
+                        onChange={handleAddressChange}
+                        className='w-full border p-2 rounded-lg focus:ring-2 focus:ring-cyan-600 outline-none'
+                    />
+                </fieldset>
+
+                {/* Cidade */}
+                <fieldset>
+                    <label htmlFor='city' className='block text-sm font-medium mb-1'>Cidade</label>
+                    <input
+                        type='text'
+                        name='city'
+                        id='city'
+                        value={formData.address.city}
+                        onChange={handleAddressChange}
+                        className='w-full border p-2 rounded-lg focus:ring-2 focus:ring-cyan-600 outline-none'
+                    />
+                </fieldset>
+
+                {/* Estado */}
+                <fieldset>
+                    <label htmlFor='state' className='block text-sm font-medium mb-1'>Estado</label>
+                    <input
+                        type='text'
+                        name='state'
+                        id='state'
+                        value={formData.address.state}
+                        onChange={handleAddressChange}
+                        disabled="true"
+                        className='w-full border p-2 rounded-lg focus:ring-2 focus:ring-cyan-600 outline-none'
+                    />
+                </fieldset>
+
+                {/* Botão de envio **TAL DA MELHORIA??*/}
+                <div className='flex justify-end gap-3 pt-4'>
+                    <button
+                        type='submit'
+                        disabled={isSaving}
+                        className='px-4 py-2 bg-cyan-700 text-white rounded-lg hover:bg-cyan-600 disabled:opacity-50'
+                    >
+                        {isSaving ? "Salvando..." : "Salvar"}
+                    </button>
+                </div>
+
             </div>
+
+
         </form>
     )
 }
